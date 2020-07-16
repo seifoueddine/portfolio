0d362361cea53f0a9b74f14a803ecf12d9350b48
seif eddine <nouaraseifeddine@gmail.com>
2020-07-16 12:00:44 +0100

    add google analitics

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from "./containers/Main";
 import ReactGA from 'react-ga';
 function App() {
 
-    ReactGA.initialize('G-NQ1S5EE5L4');
+    ReactGA.initialize('UA-172859286-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
