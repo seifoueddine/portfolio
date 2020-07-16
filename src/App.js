0977ab605616ec00980187935f9890f460a3ed65
seif eddine <nouaraseifeddine@gmail.com>
2020-07-16 11:39:24 +0100

    change tracking id

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from "./containers/Main";
 import ReactGA from 'react-ga';
 function App() {
 
-    ReactGA.initialize('237901414');
+    ReactGA.initialize('G-NQ1S5EE5L4');
     ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
