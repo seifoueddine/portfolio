939508246e22d0008da63a4f4ae83840ec529448
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2024-04-26 15:26:18 +0200

    add react ga4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga4";
+const TRACKING_ID = "G-0S9SHPZK7V";
+// import ReactGA from 'react-ga';
 function App() {
 
-    ReactGA.initialize('UA-172859286-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    // ReactGA.initialize('UA-172859286-1');
+    // ReactGA.pageview(window.location.pathname + window.location.search);
+
+      ReactGA.initialize(TRACKING_ID);
+
 
   return (
     <div>
